47536348b6fbb925aa9f02c95128a65532b58866
arturwebdev <hakobjanyan61@gmail.co>
2024-10-30 22:16:25 +0300

    new kommit

---
+++ src/components/main/Main.js
@@ -0,0 +1,11 @@
+import { useEffect } from "react"
+import axios from "axios";
+
+export default function Main() {
+
+    return (
+        <div className="main">
+            <h1>Main</h1>
+        </div>
+    )
+}
